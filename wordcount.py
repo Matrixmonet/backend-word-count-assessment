ccadbc564bb10664e99bedd5324d38e2f50d4ac6
Matrixmonet <successismyne@gmail.com>
2020-02-16 17:00:16 -0500

    Initial Commit

--- wordcount.py
+++ wordcount.py
@@ -38,48 +38,45 @@ Optional: define a helper function to avoid code duplication inside
 print_words() and print_top().
 
 """
-
 import sys
-
-
-def dictionary(filename):
-    count = {}
-    input_file = open(filename, 'r')
-    for line in input_file:
-        each_word = line.split()
-        for word in each_word:
-            word = word.lower()
-            if not word in count:
-                count[word] = 1
-            else:
-                count[word] = count[word] + 1
-    return count
+import re
+import collections
 
 
 def print_words(filename):
-    count = dictionary(filename)
-    each_word = sorted(count.keys())
-    for word in each_word:
-        print word, count[word]
+    frequency = {}
+    document_text = open(filename, 'r')
+    text_string = document_text.read().lower()
+    match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
 
+    for word in match_pattern:
+        count = frequency.get(word, 0)
+        frequency[word] = count + 1
 
-def get_count(word_count_tuple):
-    return word_count_tuple[1]
+    frequency_list = frequency.keys()
+
+    for words in frequency_list:
+        print(words, frequency[words])
 
 
 def print_top(filename):
-    count = dictionary(filename)
+    words = re.findall('\w+', open(filename).read().lower())
+    print collections.Counter(words).most_common(20)
+# +++your code here+++
+# Define print_words(filename) and print_top(filename) functions.
+# You could write a helper utility function that reads a file
+# and builds and returns a word/count dict for it.
+# Then print_words() and print_top() can just call the utility function.
 
-    items = sorted(count.items(), key=get_count, reverse=True)
+###
 
-    for item in items[:20]:
-        print item[0], item[1]
+# This basic command line argument parsing code is provided and
+# calls the print_words() and print_top() functions which you must define.
 
 
 def main():
-    print len(sys.argv)
     if len(sys.argv) != 3:
-        print 'usage: ./wordcount.py {--count | --topcount} file'
+        print 'usage: python wordcount.py {--count | --topcount} file'
         sys.exit(1)
 
     option = sys.argv[1]
